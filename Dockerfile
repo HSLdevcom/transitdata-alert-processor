904a279ba268974cc2cc65bf822159aa83c9a657
Jaakko Malkki <Jaakko.Malkki@hsl.fi>
2022-08-18 12:30:59 +0300

    Migrate to Java 11 and Temurin based Docker images

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,6 @@
-FROM openjdk:8-jre-slim
-
+FROM eclipse-temurin:11-alpine
 #Install curl for health check
-RUN apt-get update && apt-get install -y --no-install-recommends curl
+RUN apk add --no-cache curl
 
 ADD target/transitdata-alert-processor-jar-with-dependencies.jar /usr/app/transitdata-alert-processor.jar
 COPY start-application.sh /
--- start-application.sh
+++ start-application.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 
 if [[ "${DEBUG_ENABLED}" = true ]]; then
-  java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -Xms256m -Xmx1024m -jar /usr/app/transitdata-alert-processor.jar
+  java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -XX:InitialRAMPercentage=10.0 -XX:MaxRAMPercentage=95.0 -jar /usr/app/transitdata-alert-processor.jar
 else
-  java -Xms256m -Xmx1024m -jar /usr/app/transitdata-alert-processor.jar
+  java -XX:InitialRAMPercentage=10.0 -XX:MaxRAMPercentage=95.0 -jar /usr/app/transitdata-alert-processor.jar
 fi
